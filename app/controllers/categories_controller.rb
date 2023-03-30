87adc26f13e44bd39efea888bba3f4f30cee515c
Chaitanya31612 <guptachaitanya31@gmail.com>
2023-03-30 15:14:00 +0530

    categories page done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)
+    @categories = Category.paginate(page: params[:page], per_page: 6)
   end
 
   def show
