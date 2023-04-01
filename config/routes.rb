6a75b138bd5dbbe6e285d39c467e6521c0b64d2b
Chaitanya31612 <guptachaitanya31@gmail.com>
2023-04-01 17:15:51 +0530

    community final

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
 
   resources :messages, only: [:new, :create, :index]
 
-  get '/messages', to: redirect('/community')
   resources :messages, path: 'community', only: [:new, :create, :index]
 
 end
